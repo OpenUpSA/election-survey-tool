e0f20619b14398f49e59ff5e471bab31ec80390f
Riaan Snyders <riaan.snyders@gmail.com>
2018-11-13 14:44:38 +0200

    13 Nov 18 CL 1
    *  RS#3 Add Webpack
            * Added Uglify plugin
            * Added glob to bundle all JS
            * Add Babel and Css Loaders
    * RS#4 Add Workbox to replace the current `service-worker.js`
            * Installed Workbox plugin
            * Removed index.js / old service worker as this now done via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
+const glob = require('glob');
+const {GenerateSW} = require('workbox-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const entryArray = glob.sync('./src/**/*.js|.css');
+const entryObject = entryArray.reduce((acc, item) => {
+  const name = item.replace('/index.js', '');
+  acc[name] = item;
+  return acc;
+}, {});
 
 module.exports = {
-  entry: './src/index.js',
+  entry: entryObject,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -14,8 +25,28 @@ module.exports = {
         loader: 'jshint-loader'
 
       }
-   ]
+   ],
+	 loaders: [
+       {
+         test: /\.jsx?$/,
+         loader: 'babel-loader'
+       },
+       {
+         test: /\.s?css$/,
+         loaders: ['style', 'css', 'sass', 'postcss-loader']
+       },
  },
+ plugins: [
+	 new GenerateSW({
+		 importWorkboxFrom: 'local'
+	 }),
+	 new webpack.optimize.UglifyJsPlugin({
+		 compress: {
+			 warnings: false,
+			 drop_console: false,
+		 }
+	 })
+ ],
  resolve: {
    extensions: ['', '.js']
  },
